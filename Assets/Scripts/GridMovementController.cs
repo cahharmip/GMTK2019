a1c643bb8b1b4f28f85d8ba5bb19958341ed60ca
sasinn98 <sasinn98@gmail.com>
2019-08-03 19:14:36 +0700

    Created GameV2

---
+++ Assets/Scripts/GridMovementController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private WorldGridController world;
+
+    private WorldGridNode node;
+
+    private void Start()
+    {
+        node = world.root;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+					if (node.right != null) node = node.right;
+        } else if (Input.GetKeyDown(KeyCode.A))
+				{
+					if (node.left != null) node = node.left;
+				}
+				if (Input.GetKeyDown(KeyCode.W))
+        {
+					if (node.up != null) node = node.up;
+        } else if (Input.GetKeyDown(KeyCode.S))
+				{
+					if (node.down != null) node = node.down;
+				}
+				transform.position = node.transform.position;
+    }
+}
---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour
+{
+    private void Update()
+    {
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.tag.Equals("Wall"))
+            {
+                Transform wall = hit.collider.transform;
+                Vector3 localHitPosition = hit.point - wall.position;
+
+								// Send signal to adjacent wall in order to generate the laser ray in next room
+                Debug.Log(localHitPosition);
+            }
+        }
+    }
+}
