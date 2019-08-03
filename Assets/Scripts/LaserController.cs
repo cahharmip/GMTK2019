f4a4dfee02a1398da4ed688b7ebf881d41e72232
sasinn98 <sasinn98@gmail.com>
2019-08-04 03:10:34 +0700

    Replaced portals with reflectors

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -4,36 +4,87 @@ using UnityEngine;
 
 public class LaserController : MonoBehaviour
 {
-  private Vector3 _CurrentPosition;
-  private Vector3 _CurrentHitPoint;
-  private GameObject _CurrentBeam;
-  private void Update()
-  {
-    Physics.Raycast(transform.position, transform.forward, out RaycastHit hit);
-    if (hit.collider)
+    private Vector3 _CurrentPosition;
+    private Vector3 _CurrentHitPoint;
+    private LineRenderer laser;
+    private int laserVertices;
+
+    private void Start()
     {
-      if (_CurrentPosition != transform.position || _CurrentHitPoint != hit.point)
-      {
-        if (_CurrentBeam != null) Destroy(_CurrentBeam);
         GameObject lineObj = Instantiate(ResourceLoadManager.LoadGameObject("Prefabs/LaserBeam"), transform.position, Quaternion.identity);
-        LineRenderer line = lineObj.GetComponent<LineRenderer>();
-        line.SetPosition(0, transform.position);
-        line.SetPosition(1, hit.point);
-        _CurrentPosition = transform.position;
-        _CurrentHitPoint = hit.point;
-        _CurrentBeam = lineObj;
-      }
+        laser = lineObj.GetComponent<LineRenderer>();
     }
 
-    if (hit.collider.tag.Equals("Wall"))
+    private void Update()
     {
-      Transform wall = hit.collider.transform;
-      Vector3 localHitPosition = hit.point - wall.position;
-      wall.GetComponent<Portal>().Received(localHitPosition);
+        CastLaser();
+        // Physics.Raycast(transform.position, transform.forward, out RaycastHit hit);
+        // if (hit.collider)
+        // {
+        //     // if (_CurrentPosition != transform.position || _CurrentHitPoint != hit.point)
+        //     // {
+        //     //     if (_CurrentBeam != null) Destroy(_CurrentBeam);
+        //     //     GameObject lineObj = Instantiate(ResourceLoadManager.LoadGameObject("Prefabs/LaserBeam"), transform.position, Quaternion.identity);
+        //     //     LineRenderer line = lineObj.GetComponent<LineRenderer>();
+        //     //     line.SetPosition(0, transform.position);
+        //     //     line.SetPosition(1, hit.point);
+        //     //     _CurrentPosition = transform.position;
+        //     //     _CurrentHitPoint = hit.point;
+        //     //     _CurrentBeam = lineObj;
+        //     // }
+
+        //     if (hit.collider.tag.Equals("Wall"))
+        //     {
+        //         Transform wall = hit.collider.transform;
+        //         Vector3 localHitPosition = hit.point - wall.position;
+        //         Quaternion rayRotation = transform.rotation;
+        //         wall.GetComponent<Portal>().Received(localHitPosition, rayRotation);
+        //     }
+        //     else if (hit.collider.tag.Equals("Player"))
+        //     {
+        //         Debug.Log("<color=red>You're ded. noob xD</color>");
+        //     }
+        // }
     }
-    else if (hit.collider.tag.Equals("Player"))
+
+    private void CastLaser()
     {
-      Debug.Log("<color=red>You're ded. noob xD</color>");
+        laserVertices = 1;
+        laser.SetPosition(laserVertices++, transform.position);
+        CastLaser(transform.position, transform.forward, 25);
     }
-  }
+
+    private void CastLaser(Vector3 start, Vector3 direction, int limit)
+    {
+        if (limit == 0) return;
+        Physics.Raycast(start, direction, out RaycastHit hit);
+        if (hit.collider)
+        {
+            if (hit.collider.tag.Equals("Wall"))
+            {
+
+                // Transform portal = hit.collider.transform;
+                // Transform nextPortal = portal.GetComponent<Portal>().linkedPortal.transform;
+                Vector3 nextStart = hit.point;
+                Vector3 nextDirection = Vector3.Reflect(direction, hit.transform.up);
+
+                Debug.Log("Reflected: " + direction + " to " + nextDirection);
+
+                laser.positionCount = laserVertices + 1;
+                laser.SetPosition(laserVertices++, hit.point);
+
+                limit -= 1;
+                CastLaser(nextStart, nextDirection, limit);
+            }
+            else if (hit.collider.tag.Equals("Player"))
+            {
+                Debug.Log("<color=red>You're ded. noob xD</color>");
+            }
+        }
+        else
+        {
+            laser.positionCount = laserVertices + 1;
+            laser.SetPosition(laserVertices++, start + direction * 10f);
+        }
+    }
 }
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-  [SerializeField]
-  private Portal _LinkedPortal;
+    [SerializeField]
+    public Portal linkedPortal;
 
-  private Dictionary<int, GameObject> _LaserList = new Dictionary<int, GameObject>();
+    private Dictionary<int, GameObject> _LaserList = new Dictionary<int, GameObject>();
 
-  private const float OFFSET = 0.5f;
-  public void Received(Vector3 hitPosition)
-  {
-    _LinkedPortal.LaserForward(gameObject.GetInstanceID(), hitPosition);
-  }
+    private const float OFFSET = 0.5f;
+    public void Received(Vector3 hitPosition, Quaternion rayRotation)
+    {
+        Debug.Log("Received Laser", gameObject);
+        Debug.Log("Forwarding Laser", linkedPortal.gameObject);
+        Quaternion diff = transform.rotation * Quaternion.Inverse(linkedPortal.transform.rotation);
+        rayRotation = diff * rayRotation;
+        linkedPortal.LaserForward(gameObject.GetInstanceID(), hitPosition, rayRotation);
+    }
 
-  private void LaserForward(int id, Vector3 hitPosition)
-  {
-    if (_LaserList.ContainsKey(id)) return;
-    GameObject obj = ResourceLoadManager.LoadGameObject("Prefabs/BeamOrigin");
-    _LaserList[id] = obj;
-    if (obj != null) Instantiate(obj, hitPosition + transform.position + new Vector3(0,0,OFFSET), Quaternion.identity);
-  }
+    private void LaserForward(int id, Vector3 hitPosition, Quaternion rayRotation)
+    {
+        if (_LaserList.ContainsKey(id)) return;
+        GameObject obj = ResourceLoadManager.LoadGameObject("Prefabs/BeamOrigin");
+        _LaserList[id] = obj;
+        if (obj != null)
+        {
+          GameObject newBeam = Instantiate(obj, hitPosition + transform.position, Quaternion.identity);
+          //newBeam.transform.Translate(Vector3.forward * OFFSET);
+          newBeam.transform.rotation = rayRotation;
+        }
+    }
 }
