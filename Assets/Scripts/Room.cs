77a94e1b08686cbe7bc82671b7bce579d03189ef
Sorapol <sorapol57@gmail.com>
2019-08-03 18:34:29 +0700

    Added : mock up room and portal.

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour //ประตูห้อง จะเรียงลำดับ 0 - 3 วนตามเข็มนาฬิกาเสมอ
+{
+  [SerializeField]
+  private uint _RoomID;
+  [SerializeField]
+  private Room[] _AdjacentRoom;
+  [SerializeField]
+  private Portal[] _Portals;
+}
