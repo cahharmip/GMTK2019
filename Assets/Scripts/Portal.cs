0a8fd10147f9ad87140917996e7e239fbfce9b70
Sorapol <sorapol57@gmail.com>
2019-08-03 21:28:54 +0700

    fixed : infinite loop beam.

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -10,12 +10,13 @@ public class Portal : MonoBehaviour
   private const float OFFSET = 0.5f;
   public void Received(Vector3 hitPosition)
   {
+    Debug.Log("recieved");
     _LinkedPortal.LaserForward(hitPosition);
   }
 
   private void LaserForward(Vector3 hitPosition)
   {
-    GameObject obj = ResourceLoadManager.LoadGameObject("Prefabs/Beam");
+    GameObject obj = ResourceLoadManager.LoadGameObject("Prefabs/BeamOrigin");
     if (obj != null) Instantiate(obj, hitPosition + transform.position + new Vector3(0,0,OFFSET), Quaternion.identity);
   }
 }
