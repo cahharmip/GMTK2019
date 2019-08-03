c9a8bc6eaa12d2a87d7cc96562b8672caba16cc7
Sorapol <sorapol57@gmail.com>
2019-08-03 14:59:47 +0700

    Added : mock up obstacle behavior.

---
+++ Assets/Scripts/AddForceX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceX : MonoBehaviour
+{
+  // Start is called before the first frame update
+  protected void Start()
+  {
+    Debug.Log("Me feel the force.");
+    gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-20f,0,0));
+  }
+}
---
+++ Assets/Scripts/ObstrucleBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstrucleBase : MonoBehaviour
+{
+  protected void OnCollisionEnter(Collision col)
+  {
+    Debug.Log("Hey I'm just touched by " + col.gameObject.name + " senpai~~ Kiya");
+    gameObject.GetComponent<Renderer>().material.color = new Color(1, 0, 0);
+  }
+}
