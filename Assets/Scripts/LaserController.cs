2269445825c5e00935c5346665da1b288e415566
Sorapol <sorapol57@gmail.com>
2019-08-03 22:15:40 +0700

    Fixed : non stop create laser.

--- Assets/Scripts/LaserController.cs
+++ Assets/Scripts/LaserController.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class LaserController : MonoBehaviour
 {
+  private Vector3 _CurrentPosition;
+  private Vector3 _CurrentHitPoint;
+
   private void Update()
   {
     Physics.Raycast(transform.position, transform.forward, out RaycastHit hit);
     if (hit.collider)
     {
-      LineRenderer line = Instantiate(ResourceLoadManager.LoadGameObject("Prefabs/LaserBeam"), transform.position, Quaternion.identity).GetComponent<LineRenderer>();
-      line.SetPosition(0, transform.position);
-      line.SetPosition(1, hit.point);
+      if (_CurrentPosition != transform.position || _CurrentHitPoint != hit.point)
+      {
+        LineRenderer line = Instantiate(ResourceLoadManager.LoadGameObject("Prefabs/LaserBeam"), transform.position, Quaternion.identity).GetComponent<LineRenderer>();
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, hit.point);
+        _CurrentPosition = transform.position;
+        _CurrentHitPoint = hit.point;
+      }
     }
-     if (hit.collider.tag.Equals("Wall"))
-     {
-       Transform wall = hit.collider.transform;
-       Vector3 localHitPosition = hit.point - wall.position;
-       wall.GetComponent<Portal>().Received(localHitPosition);
-     }
-     else if (hit.collider.tag.Equals("Player"))
-     {
-       Debug.Log("<color=red>You're ded. noob xD</color>");
-     }
-     else
-     {
-       Debug.Log("Me ending..");
-     }
+
+    if (hit.collider.tag.Equals("Wall"))
+    {
+      Transform wall = hit.collider.transform;
+      Vector3 localHitPosition = hit.point - wall.position;
+      wall.GetComponent<Portal>().Received(localHitPosition);
+    }
+    else if (hit.collider.tag.Equals("Player"))
+    {
+      Debug.Log("<color=red>You're ded. noob xD</color>");
+    }
   }
 }
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,15 +7,19 @@ public class Portal : MonoBehaviour
   [SerializeField]
   private Portal _LinkedPortal;
 
+  private Dictionary<int, GameObject> _LaserList = new Dictionary<int, GameObject>();
+
   private const float OFFSET = 0.5f;
   public void Received(Vector3 hitPosition)
   {
-    _LinkedPortal.LaserForward(hitPosition);
+    _LinkedPortal.LaserForward(gameObject.GetInstanceID(), hitPosition);
   }
 
-  private void LaserForward(Vector3 hitPosition)
+  private void LaserForward(int id, Vector3 hitPosition)
   {
+    if (_LaserList.ContainsKey(id)) return;
     GameObject obj = ResourceLoadManager.LoadGameObject("Prefabs/BeamOrigin");
+    _LaserList[id] = obj;
     if (obj != null) Instantiate(obj, hitPosition + transform.position + new Vector3(0,0,OFFSET), Quaternion.identity);
   }
 }
