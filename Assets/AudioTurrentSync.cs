3cc600f8861a3a8f56ea8b8bd3e7eb3b6aafb7f6
pondpiu <peerawas.a@gmail.com>
2019-08-04 15:37:45 +0700

    fix timing

--- Assets/AudioTurrentSync.cs
+++ Assets/AudioTurrentSync.cs
@@ -25,7 +25,7 @@ public class AudioTurrentSync : MonoBehaviour
     {
         float timing = audioSource.time;
 
-        if(timing-lastTrigger > 1)
+        if(timing-lastTrigger >= 0.875)
         {
             AllJump();
             lastTrigger = timing;
