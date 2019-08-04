b0468dd329ed611b1a13a1bdbd77c20aaeaf9291
Sorapol <sorapol57@gmail.com>
2019-08-04 22:57:36 +0700

    Added : GoalPoint

---
+++ Assets/Scripts/GoalPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalPoint : MonoBehaviour
+{
+  [SerializeField]
+  private string _Stage;
+  protected void OnCollisionEnter(Collision collision)
+  {
+    if (collision.gameObject.tag.Equals("Player"))
+    {
+      SceneManager.LoadScene(_Stage);
+    }
+  }
+}
