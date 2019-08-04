ef8b8aa5744827ec780725d9ad956c7e98e36d5f
Sorapol <sorapol57@gmail.com>
2019-08-04 19:35:21 +0700

    Added : cinemachine

---
+++ Assets/Scripts/StageOneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class StageOneController : MonoBehaviour
+{
+  [SerializeField]
+  private CinemachineBrain cinemachine;
+
+  public enum State
+  {
+    Intro,
+    Play,
+    Pause,
+  }
+
+  public static State Stage = State.Intro;
+  protected void Awake()
+  {
+    //Do some wow animation
+  }
+}
